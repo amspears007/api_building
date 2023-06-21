955446dd19319ddb70e404bfccf49030ccf7df81
Amy Spears <amspears007@gmail.com>
2023-06-21 15:02:54 -0600

    Add tests to update a book and create instances names for id. previous name book params, and specific book

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -69,17 +69,25 @@ describe "Books API"  do
     post "/api/v1/books", headers: headers, params: JSON.generate(book: book_params)
     created_book = Book.last
 
-    # require 'pry'; binding.pry
     expect(response).to be_successful
     expect(created_book.title).to eq(book_params[:title])
     expect(created_book.author).to eq(book_params[:author])
     expect(created_book.summary).to eq(book_params[:summary])
     expect(created_book.genre).to eq(book_params[:genre])
     expect(created_book.number_sold).to eq(book_params[:number_sold])
-    # expect(created_book).to have_key(:title)
-    # expect(book[:title]).to eq("Murder on the Orient Express")
-    # expect(book[:title]).to eq(book_params[:title])
-    # expect(book[:author].to eq(book_params[:author]))
+  end
 
+  it "can update an existing book" do
+    id = create(:book).id
+    previous_name = Book.last.title
+    book_params = {title: "Charlotte's Web"}
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/books/#{id}", headers: headers, params: JSON.generate({book: book_params})
+    book = Book.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(book.title).to_not eq(previous_name)
+    expect(book.title).to eq("Charlotte's Web")
   end
 end
